06459aa5efbdc2466ca6b432641663108c4ab5cb
Tanish Kushwaha <tanishkushwahatk@gmail.com>
2024-08-01 22:01:23 +0530

    Add API key import from the env variable

--- src/getWeatherData.js
+++ src/getWeatherData.js
@@ -1,4 +1,5 @@
-import { key } from './keys/api-key.json';
+// const key = process.env.OPEN_WEATHER_API_KEY;
+const key = import.meta.env.VITE_OPENWEATHER_API_KEY;
 
 const getWeatherData = async (city) => {
   const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}&units=metric`)
